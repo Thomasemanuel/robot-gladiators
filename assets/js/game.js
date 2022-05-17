c59a5a6f8cb8f23235272948b6a1211aac00057a
Tom Emanuel <104587898+Thomasemanuel@users.noreply.github.com>
2022-05-17 13:38:04 -0400

    html added and js started

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+function fight() {
+    window.alert("The fight has begun!");
+  }
+//fight();
+  
+var playername= window.prompt("What is your robot's name?");
+// alert to show name once it is created
+console.log(playername);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playername);
